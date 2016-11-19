c4f1584e713faa6991af1e845be38603eeaadcf7
Hottinger <hottinger.jeremy@gmail.com>
2016-11-19 21:01:34 +0100

    new parser

--- server/src/app.js
+++ server/src/app.js
@@ -1,53 +1,57 @@
 // @flow
 
 import express from 'express';
-import fileUpload from 'express-fileupload';
-import _ from 'lodash';
+var bodyParser = require('body-parser')
+import imageProcess from './tesseract';
+import fs from 'fs'
 
 const PORT = 3000;
 
 const app = express();
 
-app.use(fileUpload());
+console.log("dirname", fs.__dirname);
+
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+    extended: true
+}));
 
 app.get('/', (req, res) => {
-  res.send('hello');
+    res.send('hello');
 });
 
 app.post('/image', (req, res) => {
 
-  var image;
 
-  if (!req.files) {
-    res.send('No files were uploaded.');
-    return;
-  }
 
-  image = req.files.image;
-  name = image.name;
-  image.mv('/images/'+name, function(err) {
-    if (err) {
-      res.status(500).send(err);
+    var url = req.body.image;
+    console.log(req.body);
+
+
+    /* var image;
+    const folder = 'images/';
+
+    if (!req.files) {
+        res.send('No files were uploaded.');
+        return;
     }
-    else {
-      res.send('File uploaded!');
-    }
-  });
 
-  res.send('route');
+    image = req.files.image;
+    const path = folder + image.name;
+    image.mv(path, function (err) {
+        if (err) {
+            res.status(500).send(err);
+            console.log(err);
+        }
+        else {
+            res.send('File uploaded!');
+            console.log("File uploaded");
+            imageProcess(path);
+        }
+    }); */
+
+    // console.log("debug");
 });
 
 app.listen(PORT, () => {
-  console.log('Running...');
+    console.log('Running...');
 });
-
-
-/*
-{image:
-    {name: '40DBA53A-0F24-4365-9BD3-4E740FF81C4F.jpg',
-     data: <Buffer ff d8 ff e0 00 10 4a 46 49 46 00 01 01 00 00 48 00 48 00 00 ff e1 00 58 45 78 69 66 00 00 4d 4d 00 2a 00 00 00 08 00 02 01 12 00 03 00 00 00 01 00 01 ... >,
-     encoding: '7bit',
-     mimetype: 'image/jpeg',
-     mv: [Function] }
-}
- */
--- server/src/tesseract.js
+++ server/src/tesseract.js
@@ -1,110 +1,162 @@
 import Tesseract from 'tesseract.js'
 let request = require('request')
 let fs = require('fs')
-let url = 'https://cloud.avalan.ch/s/DJoo6g4dL7gEMct/download'
+// let url = 'https://cloud.avalan.ch/s/DJoo6g4dL7gEMct/download'
 let filename = 'menu.png'
-
-
 import _ from "lodash";
 
+/* export default function imageProcess(imagePath) {
 
-/*let writeFile = fs.createWriteStream(filename)
+ // let writeFile = fs.createWriteStream(filename)
 
-request(url).pipe(writeFile).on('close', function () {
-    console.log(url, 'saved to', filename)
-    Tesseract.recognize(filename, {
-        lang: 'fra',
-        textord_tabfind_show_columns: 1
-    }).progress(function (p) {
-        console.log('progress', p)
-    })
-        .catch(err => console.error(err))
-        .then(function (result) {
+ // request(imagePath).pipe(writeFile).on('close', function () {
+ // console.log(imagePath, 'saved to', filename)
+ console.log("image path : ", imagePath);
+ Tesseract.recognize(imagePath, {
+ lang: 'fra',
+ textord_tabfind_show_columns: 1
+ }).progress(function (p) {
+ console.log('progress', p)
+ })
+ .catch(err => console.error(err))
+ .then(function (result) {
 
-            console.log(result.text)
-            const meals = parse(result.text);
-            console.log(meals);
+ console.log(result.text)
+ const meals = parse(result.text);
+ console.log("meals", meals);
 
-            process.exit(0)
-        })
-}); */
+ process.exit(0)
+ })
+ //});
 
-// const text = "MARGHERITA 16.50\n S auce tomate maison et mozzarella. \n\nRUCO LA 1 7,00\nRoquette, parmesan, sauce tomate maison.\nSALAME 17.00\nSalami Napoli sur notre sauce tomate\n\nmaison et mozzarella.\n\nFUNGHI 17,00\nChampignons frais, sauce tomate\n\nmaison et mozzarella.";
-const text = "PESTO CON SPINACI 16,50\n Jeunes pousses d‘épinards fraîches et tomates\nmarinées sur une sauce crémeuse au pesto\net mozzarella.\n \n   l avec saumon fumé + 3,90\n\nl avec crevettes + 3.90\nBRUSCHETTA 19.00\nTomates marinées dans un mélange d‘huile d‘olive\net d‘ail avec roquette, Grana Padano D.O.P., sur\nnotre sauce tomate maison et mozzarella.\nVERDURE 1 9.50\nLégumes grillés maison, champignons frais,\n3m“ notre sauce tomate et mozzarella.";
-console.log(text);
-const meals = parse(text);
-console.log(meals);
+ } */
 
-function parse(plaintext) {
-    const lines = plaintext.split("\n").map(x => x.trim()).filter(x => x !== '');
+const text = "MARGHERITA\nuce totnate maison et tnozzarella.\nRUCOLA\nRoquette, partnesan, sauce totnate maison.\nSALAME\nSalami Napoli sur notre sauce tomate\nmaison et mozzarella.\nFUNGHI\nChampignons frais, sauce tomate\nmaison et mozzarella.\n\n16,50\n17,00\n17,00\n17,00";
 
-    let startingLines = lines.map(x => {
+parse(text);
 
-        let i = 0;
-        let res = isNaN(parseInt(x.charAt(i)));
-        while(isNaN(parseInt(x.charAt(i))) && res) {
-            res = x.charAt(i) == x.charAt(i).toUpperCase();
-            i++;
+function parse(text) {
+
+    const lines = text.split("\n").filter(x => x !== '');
+
+    var array = [];
+
+    let name = "";
+    let description = [];
+    let mealCount = 0;
+    let priceCount = 0;
+    for (let s in lines) {
+        let line = lines[s];
+        let isan = !isNaN(parseFloat(line));
+        if (!isan && line.toUpperCase() === line) {
+
+            if (mealCount > 0) {
+                array.push({name, description});
+                name = "";
+                description = [];
+                priceCount = 0;
+            }
+
+            name = line;
+            mealCount++;
+        } else if (!isan) {
+            line.split(" ").forEach(x => description.push(x));
         }
 
-        return res;
-    });
-
-    let meals = [];
-    let meal = [];
-    for (let i = 0; i < lines.length; i++) {
-        if (startingLines[i]) {
-            if (i != 0) {
-                meals.push(meal);
-                meal = [];
+        if (isan) {
+            if (array[priceCount]) {
+                array[priceCount].price = parseFloat(line);
+                priceCount++;
             }
         }
-        meal.push(lines[i]);
-    }
-    meals.push(meal);
 
-    let parsedMeals = meals.map(meal => {
-        const parsedMeal = splitMeal(meal)
-        return correctPrice(parsedMeal);
-    });
-
-    // console.log(parsedMeals);
-    return parsedMeals;
-}
-
-function splitMeal(meal) {
-    const nameAndPriceLine = meal[0];
-    const descriptionLine = meal.slice(1);
-
-    const words = nameAndPriceLine.split(" ");
-    const dirtyPrice = words[words.length - 1];
-    const price = parseFloat(dirtyPrice.replace(",", ".").replace(" ", ""));
-    const name = nameAndPriceLine.slice(0, nameAndPriceLine.length - dirtyPrice.length).trim();
-
-    let tmpDescription = [];
-    descriptionLine.forEach(line => {
-        const cleanedWords = line.split(" ").map(word => word.trim());
-        tmpDescription.push(cleanedWords);
-    });
-
-    const description = _.flatten(tmpDescription);
-
-    return {name, price, description}
-}
-
-function correctPrice(meal) {
-
-    const nameWords = meal.name.split(" ");
-    if (isNaN(nameWords[nameWords.length - 1]))
-        return meal;
-
-    let price = meal.price.toString();
-    let i = 1;
-    while (!isNaN(parseInt(nameWords[nameWords.length - i]))) {
-        price = parseInt(nameWords[nameWords.length - i]) + price;
-        i++;
     }
 
-    return {name: nameWords.slice(0, i), price: parseFloat(price), description: meal.description}
+    console.log(array);
 
 }
+
+
+/*
+ */
+
+
+// const text = "MARGHERITA 16.50\n S auce tomate maison et mozzarella. \n\nRUCO LA 1 7,00\nRoquette, parmesan, sauce tomate maison.\nSALAME 17.00\nSalami Napoli sur notre sauce tomate\n\nmaison et mozzarella.\n\nFUNGHI 17,00\nChampignons frais, sauce tomate\n\nmaison et mozzarella.";
+// const text = "PESTO CON SPINACI 16,50\n Jeunes pousses d‘épinards fraîches et tomates\nmarinées sur une sauce crémeuse au pesto\net mozzarella.\n \n   l avec saumon fumé + 3,90\n\nl avec crevettes + 3.90\nBRUSCHETTA 19.00\nTomates marinées dans un mélange d‘huile d‘olive\net d‘ail avec roquette, Grana Padano D.O.P., sur\nnotre sauce tomate maison et mozzarella.\nVERDURE 1 9.50\nLégumes grillés maison, champignons frais,\n3m“ notre sauce tomate et mozzarella.";
+/* console.log(text);
+ const meals = parse(text);
+ console.log(meals); */
+
+/* function parse(plaintext) {
+ const lines = plaintext.split("\n").map(x => x.trim()).filter(x => x !== '');
+
+ let startingLines = lines.map(x => {
+
+ let i = 0;
+ let res = isNaN(parseInt(x.charAt(i)));
+ while (isNaN(parseInt(x.charAt(i))) && res) {
+ res = x.charAt(i) == x.charAt(i).toUpperCase();
+ i++;
+ }
+
+ return res;
+ });
+
+ let meals = [];
+ let meal = [];
+ for (let i = 0; i < lines.length; i++) {
+ if (startingLines[i]) {
+ if (i != 0) {
+ meals.push(meal);
+ meal = [];
+ }
+ }
+ meal.push(lines[i]);
+ }
+ meals.push(meal);
+
+ let parsedMeals = meals.map(meal => {
+ const parsedMeal = splitMeal(meal)
+ return correctPrice(parsedMeal);
+ });
+
+ // console.log(parsedMeals);
+ return parsedMeals;
+ }
+
+ function splitMeal(meal) {
+ const nameAndPriceLine = meal[0];
+ const descriptionLine = meal.slice(1);
+
+ const words = nameAndPriceLine.split(" ");
+ const dirtyPrice = words[words.length - 1];
+ const price = parseFloat(dirtyPrice.replace(",", ".").replace(" ", ""));
+ const name = nameAndPriceLine.slice(0, nameAndPriceLine.length - dirtyPrice.length).trim();
+
+ let tmpDescription = [];
+ descriptionLine.forEach(line => {
+ const cleanedWords = line.split(" ").map(word => word.trim());
+ tmpDescription.push(cleanedWords);
+ });
+
+ const description = _.flatten(tmpDescription);
+
+ return {name, price, description}
+ }
+
+ function correctPrice(meal) {
+
+ const nameWords = meal.name.split(" ");
+ if (isNaN(nameWords[nameWords.length - 1]))
+ return meal;
+
+ let price = meal.price.toString();
+ let i = 1;
+ while (!isNaN(parseInt(nameWords[nameWords.length - i]))) {
+ price = parseInt(nameWords[nameWords.length - i]) + price;
+ i++;
+ }
+
+ return {name: nameWords.slice(0, i), price: parseFloat(price), description: meal.description}
+
+ } */
