e4c75d85ecf8717c455213328bf02ac25c557f95
asoccard <aurelien.soccard@epfl.ch>
2016-11-19 18:21:28 +0100

    Add fileupload

--- server/src/app.js
+++ server/src/app.js
@@ -1,19 +1,21 @@
 // @flow
 
 import express from 'express';
+import fileUpload from 'express-fileupload';
 import _ from 'lodash';
 
 const PORT = 3000;
 
 const app = express();
 
+app.use(fileUpload());
+
 app.get('/', (req, res) => {
   res.send('hello');
 });
 
 app.post('/image', (req, res) => {
-  
-
+  console.log(req.files);
   res.send('route');
 });
 
