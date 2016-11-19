fb807fefe4cf2e2d4beb7db0062d93373e0a1c78
Teo Stocco <teo@zifeo.com>
2016-11-19 14:37:43 +0100

    add research

---
+++ research/OpenFood.py
@@ -0,0 +1,71 @@
+
+# coding: utf-8
+
+# In[1]:
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import word2vec
+
+
+# In[4]:
+
+products = pd.read_csv('data/products.csv', index_col=1)
+products = pd.read_csv('data/products.csv')
+
+
+# In[3]:
+
+product.head
+
+
+# In[ ]:
+
+
+
+
+# In[ ]:
+
+
+
+
+# In[ ]:
+
+
+
+
+# In[ ]:
+
+
+
+
+# In[ ]:
+
+
+
+
+# In[ ]:
+
+
+
+
+# In[ ]:
+
+
+
+
+# In[ ]:
+
+
+
+
+# In[ ]:
+
+
+
+
+# In[ ]:
+
+
+
