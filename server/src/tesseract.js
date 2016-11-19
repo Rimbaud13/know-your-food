33ebd269b8f9ddbe4bb17dfc0e22bcb5982bd4e1
Nicolas Roussel <n.roussel13@gmail.com>
2016-11-19 17:30:05 +0100

    Parse meals from tesseract

--- server/src/tesseract.js
+++ server/src/tesseract.js
@@ -1,10 +1,13 @@
-import Tesseract from 'tesseract.js'
-var request = require('request')
-var fs = require('fs')
-var url = 'https://cloud.avalan.ch/s/fbyELHPmVwXfrLM/download'
-var filename = 'menu.png'
+let request = require('request')
+let fs = require('fs')
+let url = 'https://cloud.avalan.ch/s/fbyELHPmVwXfrLM/download'
+let filename = 'menu.png'
 
-var writeFile = fs.createWriteStream(filename)
+
+import _ from "lodash";
+
+
+let writeFile = fs.createWriteStream(filename)
 
 /* request(url).pipe(writeFile).on('close', function () {
  console.log(url, 'saved to', filename)
@@ -26,21 +29,68 @@ const text = "MARGHERITA 16.50\n Sauce tomate maison et mozzarella. \n\nRUCO LA 
 const meals = parse(text);
 
 function parse(plaintext) {
-    const lines = plaintext.split("\n").map(x => x.trim()).filter(x => x !== '').filter(x => x.charAt(1) === x.charAt(1).toUpperCase());
+  const lines = plaintext.split("\n").map(x => x.trim()).filter(x => x !== '');
 
-    var meals = lines.map(x => {
-        return splitLine(x)
-    });
+  let startingLines = lines.map(x => {
+    let tmp = (_.dropWhile(x, _ == " ")).join("");
+    return tmp.charAt(1) == tmp.charAt(1).toUpperCase();
+  });
 
-    console.log(meals);
-    return meals;
+  let meals = [];
+  let meal = [];
+  for (let i = 0; i < lines.length; i++) {
+    if (startingLines[i]) {
+      if (i != 0) {
+        meals.push(meal);
+        meal = [];
+        meal = [];
+      }
+    }
+    meal.push(lines[i]);
+  }
+  meals.push(meal);
+
+  let parsedMeals = meals.map(meal => {
+    return splitMeal(meal)
+  });
+
+  console.log(parsedMeals);
+  return parsedMeals;
 }
 
-function splitLine(line) {
-    const words = line.split(" ");
-    const dirtyprice = words[words.length-1];
-    const price = parseFloat(dirtyprice.replace(",", ".").replace(" ", ""));
-    const name = line.slice(0, line.length-dirtyprice.length).trim();
+function splitMeal(meal) {
+  const nameAndPriceLine = meal[0];
+  const descriptionLine = meal.slice(1);
 
-    return {name, price}
+  const words = nameAndPriceLine.split(" ");
+  const dirtyPrice = words[words.length - 1];
+  const price = parseFloat(dirtyPrice.replace(",", ".").replace(" ", ""));
+  const name = nameAndPriceLine.slice(0, nameAndPriceLine.length - dirtyPrice.length).trim();
+
+  let tmpDescription = [];
+  descriptionLine.forEach(line => {
+    const cleanedWords = line.split(" ").map(word => word.trim());
+    tmpDescription.push(cleanedWords);
+  });
+
+  const description = _.flatten(tmpDescription);
+
+  return {name, price, description}
+}
+
+function correctPrice(meal) {
+
+  const nameWords = meal.name.split(" ");
+  if (isNaN(nameWords[nameWords.length - 1]))
+    return meal;
+
+  let price = meal.price.toString();
+  let i = 1;
+  while (!isNaN(parseInt(nameWords[nameWords.length - i]))) {
+    price = parseInt(nameWords[nameWords.length - i]) + price;
+    i++;
+  }
+
+  return {name:nameWords.slice(0, i-1), price: parseFloat(price), description:meal.description}
+
 }
