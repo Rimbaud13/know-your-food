ff90e3f636ca700e66a11f467f3dc48e6b1bd940
Teo Stocco <teo@zifeo.com>
2016-11-20 10:56:40 +0100

    deploy on swisscom

--- server/src/app.js
+++ server/src/app.js
@@ -4,7 +4,7 @@ import express from 'express';
 var bodyParser = require('body-parser')
 import imageProcess from './tesseract';
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
